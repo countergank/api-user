9897d264a894f75fc775a131b0bc45d1806af362
leandrojaviercepeda <leandrojaviercepeda@gmail.com>
2023-10-11 17:06:23 -0300

    develop
    * Refactor de getVersion

--- docker-redeploy.sh
+++ docker-redeploy.sh
@@ -2,7 +2,7 @@
 # -V force install dependencies (si hubo cambios en el package.json)
 # --build rebuilds the image
 
-NODE_ENV=local
+NODE_ENV=development
 
 if command -v docker compose; then
 	docker compose --env-file .env.$NODE_ENV down
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as fs from 'fs';
 import { VersionNotFoundError } from './common/errors/version-not-found.error';
 
 @Injectable()
@@ -9,15 +8,8 @@ export class AppService {
 
   getVersion(): string {
     try {
-      // Lee el contenido del archivo package.json
-      const packageJson = fs.readFileSync('package.json', 'utf-8');
-
-      // Analiza el archivo JSON y obtén la versión
-      const packageData = JSON.parse(packageJson);
-      const version = packageData.version;
-      const env = this.configService.get('NODE_ENV');
-
-      return `User Manager API. v${version}.${env}`;
+      const tag = this.configService.get('TAG');
+      return `User Manager API. v=${tag}`;
     } catch (error) {
       throw new VersionNotFoundError();
     }
