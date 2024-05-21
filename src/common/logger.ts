80e48831d74ca7662eb46e756c31866aa55e3547
leandrojaviercepeda <leandrojaviercepeda@gmail.com>
2024-05-20 21:40:34 -0300

    CGUM-38-refactor-errors-handling
    * refactor de logger de aplicacion

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,24 +1,35 @@
 import { ConsoleLogger } from '@nestjs/common';
 
 export class CustomLogger extends ConsoleLogger {
-  error(message: any, stack?: string, context?: string) {
-    if (process.env.NODE_ENV === 'test') return;
-    super.error(message, stack ?? '', context ?? '');
+  private debugMode: boolean;
+
+  constructor(context: string) {
+    super(context);
+    this.debugMode = process.env.DEBUG ? JSON.parse(process.env.DEBUG) : false;
   }
-  log(message: any, stack?: string, context?: string) {
-    if (process.env.NODE_ENV === 'test') return;
-    super.log(message, stack ?? '', context ?? '');
+
+  log(message: any) {
+    if (process.env.NODE_ENV === 'test' && !this.debugMode) return;
+    super.log(message, this.context);
   }
-  debug(message: any, stack?: string, context?: string) {
-    if (process.env.NODE_ENV === 'test') return;
-    super.debug(message, stack ?? '', context ?? '');
+
+  debug(message: any) {
+    if (process.env.NODE_ENV === 'test' && !this.debugMode) return;
+    super.debug(message, this.context);
   }
-  verbose(message: any, stack?: string, context?: string) {
-    if (process.env.NODE_ENV === 'test') return;
-    super.verbose(message, stack ?? '', context ?? '');
+
+  verbose(message: any) {
+    if (process.env.NODE_ENV === 'test' && !this.debugMode) return;
+    super.verbose(message, this.context);
   }
-  warn(message: any, stack?: string, context?: string) {
-    if (process.env.NODE_ENV === 'test') return;
-    super.warn(message, stack ?? '', context ?? '');
+
+  warn(message: any) {
+    if (process.env.NODE_ENV === 'test' && !this.debugMode) return;
+    super.warn(message, this.context);
+  }
+
+  error(message: any, stack?: string) {
+    if (process.env.NODE_ENV === 'test' && !this.debugMode) return;
+    super.error(message, stack, this.context);
   }
 }
